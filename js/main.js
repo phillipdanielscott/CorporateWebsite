fd10e2d9c40923e930935fd1eefaae86f8f84870
Phillip  Scott <phillipscott@Phillips-MacBook-Pro.local>
2017-02-25 21:28:05 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+    "use strict";
+
+    $('#slider-carousel').carouFredSel({
+
+        responsive: true,
+        // width: '100%',
+        circular: true,
+        scroll: {
+            items: 1,
+            duration: 500,
+            pauseOnHover: true
+        },
+        auto: true,
+        items: {
+            // width: "100%",
+            visibile: {
+                min: 1,
+                max: 1
+            },
+            height: "variable"
+        },
+        pagination: {
+            container: ".sliderpager",
+            anchorBuilder: false
+        }
+    });
+
+    $('.portfolio-carousel').carouFredSel({
+
+        responsive: true,
+        // width: '100%',
+        circular: true,
+        scroll: {
+            items: 1,
+            duration: 500,
+            pauseOnHover: true
+        },
+        auto: true,
+        items: {
+            // width: 900,
+            // width: "100%",
+            visibile: {
+                min: 1,
+                max: 4
+            },
+            height: "variable"
+        },
+    })
+
+    // console.log("is this even working?")
+
+    $(window).scroll(function() {
+
+        var top = $(window).scrollTop();
+        if (top >= 60) {
+            $("header").addClass('secondary-dark-blue-bg');
+        } else
+        if ($("header").hasClass('secondary-dark-blue-bg')) {
+            $("header").removeClass('secondary-dark-blue-bg');
+
+        }
+    })
+
+
+
+});
