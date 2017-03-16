cfc8f54856b017f7a59a6f16a5176d9bfe862952
Phillip  Scott <phillipscott@Phillips-MacBook-Pro.local>
2017-03-16 12:58:32 -0400

    hooked up domain cname to hosting website, everything is up and running!

--- js/main.js
+++ js/main.js
@@ -121,7 +121,7 @@ $(document).ready(function() {
 
     $('#main').stellar();
 
-    console.log("this is the end of the code man");
+    console.log("all the sliders are loaded");
 
 
     $('.animation').each(function() {
